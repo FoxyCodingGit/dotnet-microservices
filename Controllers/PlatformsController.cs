fe99dceee2d46db2d2c1b3d8e67ceb24443818a0
Adam <adam@eastpoint.co.uk>
2021-11-18 10:34:06 +0000

    add controler file and dockerfile

---
+++ Controllers/PlatformsController.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.PlatformsController
+{
+    [Route("api/[controller]")]
+    // out the box api functionality
+    [ApiController]
+    public class PlatformsController: ControllerBase
+    {
+        private readonly IPlatformRepo _repository;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(IPlatformRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting Platforms...");
+
+            var platformItem = _repository.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItem));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatformById(int id)
+        {
+            var platformItem = _repository.GetPlatformById(id);
+
+            if (platformItem != null)
+            {
+                return Ok(_mapper.Map<PlatformReadDto>(platformItem));
+            }
+
+           return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+        {
+            var platformModel = _mapper.Map<Platform>(platformCreateDto);
+            _repository.CreatePlatform(platformModel);
+            _repository.SaveChanges();
+            
+
+            var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+
+            // Learning-notes - return a 201 response, give the route of create by id call. Generate new Id and passing back the body payload.
+            return CreatedAtRoute(nameof(GetPlatformById), new {Id = platformReadDto.Id}, platformReadDto);
+        }
+    }
+}
--- Dtos/PlatformCreateDto.cs
+++ Dtos/PlatformCreateDto.cs
@@ -5,7 +5,7 @@ namespace PlatformService.Dtos
     public class PlatformCreateDto 
     {
         // LEARNING-NOTE: see how the id of the platform model is not set here. We are restricing the external user access to "id" on create as this is something we will do automaticallly on our side.
-
+        // If id is added in the post request, then it is simply ignored as its not mapped.
         [Required]
         public string Name { get; set; }
 
