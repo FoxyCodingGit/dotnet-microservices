b64eca0712b981732af54fac48cb1e9e418ae63a
Adam <adam@eastpoint.co.uk>
2021-11-30 09:33:06 +0000

    Add new command service with controller and action

---
+++ CommandService/Controllers/PlatformsController.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        // changed the url so it doesnt match the other service? Says to trust him and that this is future proofing!
+        public PlatformsController()
+        {
+
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> Inbound POST # Command Service");
+
+            return Ok("Inbound test of from Platforms Controller");
+        }
+    }
+}
---
+++ PlatformService/SyncDataServices/Http/HttpCommandDataClient.cs
@@ -0,0 +1,21 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using PlatformService.Dtos;
+
+namespace PlatformService.SyncDataServices.Http
+{
+    public class HttpCommandDataClient : ICommandDataClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public HttpCommandDataClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public Task SendPlatformToCommand(PlatformReadDto plat)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
