ae7c497776a7832a3fdc4970d1be0f1ea23c6c7f
Adam <adam@eastpoint.co.uk>
2021-12-07 21:17:50 +0000

    add dtos, models, automapper and data layer

---
+++ CommandService/Data/ICommandRepo.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using CommandService.Models;
+
+namespace CommandService.Data
+{
+    public interface ICommandRepo
+    {
+        bool SaveChanges();
+
+        // platforms
+        IEnumerable<Platform> GetAllPlatforms();
+
+        void CreatePlatform(Platform plat);
+
+        bool PlatformExists(int platformId);
+
+        // commands
+        IEnumerable<Command> GetCommandsForPlatform(int platformId);
+
+        Command GetCommand(int platformId, int commandId);
+
+        void CreateCommand(int platformId, Command command);
+    }
+}
